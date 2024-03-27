63473ec0d9a06d6b40196bd3753875e213347b33
Earsuit <renyudicn@outlook.com>
2024-03-27 22:36:35 +0800

    fix: correct OneDrive path on Windows

--- ImFileDialog.cpp
+++ ImFileDialog.cpp
@@ -551,7 +551,7 @@ namespace ifd {
 		m_treeCache.emplace_back(std::move(quickAccess));
 
 		// OneDrive
-		auto oneDrive = std::make_unique<FileTreeNode>(userPath + L"OneDrive");
+		auto oneDrive = std::make_unique<FileTreeNode>(getenv("OneDriveConsumer"));
 		m_treeCache.emplace_back(std::move(oneDrive));
 
 		// This PC
